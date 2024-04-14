fb48d1cf03e9826eedff6d66949386d8190ad309
Vivek <viveks95944@gmail.com>
2024-04-14 23:33:06 +0530

    port_fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path'); // Added path module for file paths
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
